8fc7ce1264cd39d765b6b2f5eac57a3e36773494
ayush <ayush@email.com>
2023-03-17 11:21:32 +0530

    entered some sample data

--- mongo.js
+++ mongo.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 let uri = 'mongodb://127.0.0.1/'
 let db_name = 'demo'
 
+// connection string
 async function main(){
     console.log('trying to connect to ' + db_name)
     await mongoose.connect(`${uri}${db_name}`)
@@ -10,6 +11,7 @@ async function main(){
 }
 main()
 
+// Schema declaration
 const PhoneSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -22,4 +24,37 @@ const PhoneSchema = new mongoose.Schema({
     },
 })
 
-const User = mongoose.model('User', PhoneSchema)
+// model declaration
+const Phone = mongoose.model('Phone', PhoneSchema)
+
+let new_entry =
+[{ 
+    "name": "Arto Hellas", 
+    "number": "040123456"
+},
+{ 
+    "name": "Ada Lovelace", 
+    "number": "39445323523"
+},
+{ 
+    "name": "Dan Abramov", 
+    "number": "1243234345"
+},
+{ 
+    "name": "Mary Poppendieck", 
+    "number": "39236423122"
+}]
+
+// modelname.insertMany(arr, function(error, docs) {});
+
+async function saveEntry(){
+try{
+        await Phone.insertMany(new_entry)
+        console.log(new_entry)
+    }catch(err){
+        console.log(err)
+    }
+    
+}
+
+saveEntry()
