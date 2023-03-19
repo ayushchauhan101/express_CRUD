af68e3481a499bce8affbdaf706a0717034fa921
ayush <ayush@email.com>
2023-03-19 20:56:03 +0530

    shift logic of mongo

---
+++ myMongoDB.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose')
+
+let uri = 'mongodb://127.0.0.1/'
+let db_name = 'demo'
+
+// connection string
+async function main(){
+    console.log('trying to connect to ' + db_name)
+    await mongoose.connect(`${uri}${db_name}`)
+    console.log('connected to local DB: '+db_name)
+}
+main()
+
+// Schema declaration
+const PhoneSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        lowercase: true,
+    },
+    number: {
+        type: Number,
+        required: true,
+    },
+})
+
+// model declaration
+const Phone = mongoose.model('Phone', PhoneSchema)
+
+let new_entry =
+[{ 
+    "name": "Arto Hellas", 
+    "number": "040123456"
+},
+{ 
+    "name": "Ada Lovelace", 
+    "number": "39445323523"
+},
+{ 
+    "name": "Dan Abramov", 
+    "number": "1243234345"
+},
+{ 
+    "name": "Mary Poppendieck", 
+    "number": "39236423122"
+}]
+
+async function saveEntry(){
+try{
+        await Phone.insertMany(new_entry)
+        console.log(new_entry)
+    }catch(err){
+        console.log(err)
+    }
+    
+}
+// saveEntry()
+
+async function searchEntry(){
+    try{
+        // let result = await Phone.find({name:"dan abramov"}).exec()
+        let result = await Phone.find({}).exec()
+        console.log(result)
+    }catch(err){
+        console.log(err)
+    }
+}
+// searchEntry()
+
+// there are teo same object ids but using only .id instead of ._id
+PhoneSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = Phone
